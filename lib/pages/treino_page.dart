2850448c35e7ae1dfb9b1aceaad052bfdbe8692a
waldson fagundes <waldsonfagundes@waldsons-Mac-mini.local>
2023-12-07 18:54:01 -0300

    TreinoPage class created

---
+++ lib/pages/treino_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class TreinoPage extends StatefulWidget {
+  const TreinoPage({super.key});
+
+  @override
+  State<TreinoPage> createState() => _TreinoPageState();
+}
+
+class _TreinoPageState extends State<TreinoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {},
+          child: const Text('Iniciar Treino'),
+        ),
+      ),
+    );
+  }
+}
